673985dc43ebc88b1e58611c86e2d7575e5ccca2
root <ksalapat@uwaterloo.ca>
2026-03-29 17:32:59 +0000

    loop the read index on ILD_REP

--- src/mem/MAA/IndirectAccess.hh
+++ src/mem/MAA/IndirectAccess.hh
@@ -126,6 +126,7 @@ protected:
     bool my_fill_finished;
     bool my_force_cache_determined;
     bool my_force_cache;
+    bool need_rep;
 
     bool my_translation_done;
     Addr my_translated_addr;
